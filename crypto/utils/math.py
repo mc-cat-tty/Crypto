c3b247414ea5c2609d88fa82f1d841e2bb0c5acb
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-03-15 09:49:14 +0100

    initial commit

---
+++ crypto/utils/math.py
@@ -0,0 +1,10 @@
+"""
+Collection of useful math functions
+"""
+
+def isprime(n: int) -> bool:
+   """
+   Check if a number is prime
+   :param n: input number
+   :return: True if n is prime
+   """
